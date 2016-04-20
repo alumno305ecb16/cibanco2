43b9dcd9ec4e6e2f75b7099774b403b2aab9dff7
alumno305ecb16 <scorpions130295@gmail.com>
2016-04-19 19:57:20 -0500

    prueba3imagen

--- js/acciones.js
+++ js/acciones.js
@@ -1,11 +1,6 @@
 $(document).ready(function(e) {	
-document.addEventListener("deviceready",function(){
+//document.addEventListener("deviceready",function(){
 
- $('#iaxxa').on('click', function (){
-     //  alert("Hola");
-	 url = $(this).attr("href");
-      window.open(url, 'https://axa.mx/home');
-      return false;
-   });
-});
+
+//});
 });
